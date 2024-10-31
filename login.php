682237a69c4ea62bf88c9dbb38225efdcd1767b7
aktivitasRudi <rudiedukasih@gmail.com>
2024-10-31 09:23:11 +0700

    file login

--- login.php
+++ login.php
@@ -1,18 +1,11 @@
-<?php
-if(!isset($_SESSION['iduser'])){
-    include"login.php";
-}else{
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tampilan Chat Room</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Room Chat</h1>
-    
+    file login
 </body>
 </html>
-<?php } ?>

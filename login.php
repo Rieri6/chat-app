5331899b99a2457205138e347c6876066d6cef9b
aktivitasRudi <rudiedukasih@gmail.com>
2024-10-31 10:15:30 +0700

    edit login

--- login.php
+++ login.php
@@ -4,8 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 </head>
 <body>
-    file login
+<div class="container text-center"> 
+  <div class="row">
+    <div class="col-md-6 offset-md-3">
+          <div class="card" style="width: 18rem;">
+        Login Here
+            <div class="card-body">
+              <h5 class="card-title">Admin Walcome</h5>
+              <form action="" method="post">
+              <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">Username</label>
+              <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Username</label>
+              <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <button type="submit" name="btn">Login</button>
+            </form>
+                </div>
+      </div>
+       
+
+    </div>
+  </div>
+</div>
+    
 </body>
 </html>
